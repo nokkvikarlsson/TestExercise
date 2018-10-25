53dfe9113989a3291ea6d64a1f9ad3f6ca7f1e96
nokkvikarlsson <nokkvi17@ru.is>
2018-10-25 14:29:03 +0000

    installed jsdoc for autogenerated documentation, ESDoc did not work

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# TestExercise
+<a name="greeting"></a>
+
+## greeting()
+This is a description of the foo function.
+
+**Kind**: global function  
--- src/logic/greeting.js
+++ src/logic/greeting.js
@@ -1,17 +1,7 @@
-
-/**
- * What a great class this is!
- * @example name is a string
- * let myClass = new MyAwesomeClass();
- */
-/**
-   * Constructor for my awesome class
-   * @param {number} foo The foo for my awesome class
-   * @param {string} bar The bar for my awesome class
-   */ 
-//Takes in a string variable as parameter and returns a greeting with that name.
+/** This is a description of the foo function. */
 function greeting(name) {
   return "Hello, " + name + "!";
 }
 
 module.exports = greeting;
+
