218e48d9c93a12e63005c20952270f828da08575
nokkvikarlsson <nokkvi17@ru.is>
2018-10-25 15:26:45 +0000

    Created a script in package.json 'doc' that documents all code in the /src/logic in markdown and puts it in the readme.md

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+## Functions
+
+<dl>
+<dt><a href="#Tictactoe">Tictactoe()</a> ⇒ <code>survival</code></dt>
+<dd><p>TicTacoToe.</p>
+</dd>
+<dt><a href="#greeting">greeting()</a></dt>
+<dd><p>This is a description of the foo function.</p>
+</dd>
+</dl>
+
+<a name="Tictactoe"></a>
+
+## Tictactoe() ⇒ <code>survival</code>
+TicTacoToe.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+|  | <code>object</code> | privacy gown |
+|  | <code>object</code> | security |
+
 <a name="greeting"></a>
 
 ## greeting()
---
+++ src/logic/Tictactoe.js
@@ -0,0 +1,91 @@
+const GameBoard = require('./GameBoard');
+const ScoreBoard = require('./ScoreBoard');
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+  })
+
+/**
+ * TicTacoToe.
+ * @param {object} - privacy gown
+ * @param {object} - security
+ * @returns {survival} 
+ */
+function Tictactoe() {
+	this.gameBoard = new GameBoard();
+	this.scoreBoard = new ScoreBoard();
+	this.sign = 'X';
+}
+
+Tictactoe.prototype.takeInput = function(input) {
+	if(this.gameBoard.isValidInput(input)) {
+		if(this.gameBoard.fillSquare(input, this.sign)) {
+			if(this.sign == 'X') {
+				this.sign = 'O';
+			}
+			else {
+				this.sign = 'X';
+			}
+			return true;
+		}
+	}
+	else {
+		return false;
+	}
+}
+Tictactoe.prototype.showBoard = function() {
+	console.log(this.gameBoard.squares[0] + "|" + this.gameBoard.squares[1] + "|" + this.gameBoard.squares[2]);
+	console.log("- - -");
+	console.log(this.gameBoard.squares[3] + "|" + this.gameBoard.squares[4] + "|" + this.gameBoard.squares[5]);
+	console.log("- - -");
+	console.log(this.gameBoard.squares[6] + "|" + this.gameBoard.squares[7] + "|" + this.gameBoard.squares[8]);
+}
+
+Tictactoe.prototype.startNewGame = function() {
+	readline.question("Would you like to start a new game? (y/n)", (input) => {
+		if(input != 'y' && input != 'n') {
+			this.startNewGame();
+		}
+		else {
+			if(input == 'y') {
+				this.sign = 'X';
+				this.gameBoard.clearBoard();
+				this.play();
+			}
+			else {
+				console.log("Goodbye");
+			}
+		}
+	})
+}
+
+Tictactoe.prototype.play = function() {
+		console.log("Scoreboard");
+		console.log("X: " + this.scoreBoard.xWins + "-" + this.scoreBoard.oWins + " :O");
+		this.showBoard();
+		console.log("choose a number from 1-9");
+		readline.question("It's " + this.sign + " turn", (input) => {
+			this.takeInput(input);
+			if(!this.gameBoard.isWinner('X') && !this.gameBoard.isWinner('O') && !this.gameBoard.isFull()) {
+				this.play();
+			}
+			else {
+				if(this.gameBoard.isFull()) {
+					console.log("Draw");
+				}
+				else if(this.gameBoard.isWinner('X')) {
+					this.scoreBoard.raiseWins('X');
+					console.log('X won!');
+				}
+				else {
+					this.scoreBoard.raiseWins('O');
+					console.log('O won');
+				}
+				this.startNewGame();
+			}
+		  })
+}
+
+/*const tic = new Tictactoe();
+tic.play();*/
+module.exports = Tictactoe;
